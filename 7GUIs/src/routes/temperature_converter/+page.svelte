eb2b744bc2ea419a5af9420d7d0ec7976159dbe8
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-03-07 02:33:09 -0300

    7GUIs
    The 7 Tasks

---
+++ 7GUIs/src/routes/temperature_converter/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let celsius = $state(0)
+    let fahrenheit = $state(conversion)
+    let conversion = $derived(celsius * (9/5) + 35)
+</script>
+
+<div>
+    <input id="celsius" type="number" bind:value={celsius}>
+    <label for="celsius">Celsius</label>
+    <span>=</span>
+    <input id="fahrenheit" type="number" bind:value={fahrenheit}>
+    <label for="fahrenheit">Fahrenheit</label>
+</div>
--- Javascript_only/Cassino.js
+++ Javascript_only/Cassino.js
@@ -1,39 +1,35 @@
-const slot = [ "Star","Moon","Sun","Eclipse" ]
-const reuslt = []
-const coin = 0
+const slot = ["Star", "Moon", "Sun", "Eclipse"]
+let result = []
 
 function spin() {
-    reuslt.length = 0
+  result.length = 0
 
-    for(let i = 0; i < 3; i++) {
-        const index = Math.floor(Math.random() * slot.length)
-        const item = slot[index]
-        reuslt.push(item)
-    }
-    
-    console.log(reuslt)
+  for (let i = 0; i < 3; i++) {
+    const index = Math.floor(Math.random() * slot.length)
+    const item = slot[index]
+    result.push(item)
+  }
 
-    if (reuslt.every(item => item === reuslt[0])) {
-        console.log("Parabens!")
-    } else if (reuslt.some(item => item === reuslt[0])) {
-        console.log("Quase!")
-    } else {
-        console.log("Não foi dessa vez!")
-    }
+  console.log(result)
+
+  if (result.every(item => item === result[0])) {
+    console.log("Parabéns!")
+  } else if (result.some(item => item === result[0])) {
+    console.log("Quase!")
+  } else {
+    console.log("Não foi dessa vez!")
+  }
 }
 
 function autoPlay(qnt) {
-   const interval = setInterval(() => {
-        if (qnt > 0) {
-            qnt--
-            spin()
-        } else {
-            clearInterval(interval)
-        }
-    }, 1000);
+  const interval = setInterval(() => {
+    if (qnt > 0) {
+      qnt--
+      spin()
+    } else {
+      clearInterval(interval)
+    }
+  }, 1000)
 }
 
-autoPlay(1)
-
-
-
+autoPlay(3)
