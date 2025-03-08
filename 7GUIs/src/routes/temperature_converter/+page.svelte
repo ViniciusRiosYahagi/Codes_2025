1dec955df4e447beb607ba4c6eb00e6244d84843
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-03-07 23:03:57 -0300

    Celsius to Fahrenheit

--- 7GUIs/src/routes/temperature_converter/+page.svelte
+++ 7GUIs/src/routes/temperature_converter/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
-    let celsius = $state(0)
-    let fahrenheit = $state(conversion)
-    let conversion = $derived(celsius * (9/5) + 35)
+    let c = $state(0)
+    let f = $state(0)
+
+    function cToF() {
+       f = c * ( 9 / 5 ) + 32 
+    }
+
+    function fToC() {
+       c = f * ( 9 / 5 ) + 32 
+    }
 </script>
 
 <div>
-    <input id="celsius" type="number" bind:value={celsius}>
+    <input id="celsius" type="number" bind:value={c} oninput={cToF}>
     <label for="celsius">Celsius</label>
     <span>=</span>
-    <input id="fahrenheit" type="number" bind:value={fahrenheit}>
+    <input id="fahrenheit" type="number" bind:value={f} oninput={fToC}>
     <label for="fahrenheit">Fahrenheit</label>
 </div>
