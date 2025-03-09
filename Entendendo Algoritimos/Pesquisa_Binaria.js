5c9a3b762925321cb9e625990c0b28a9469325ca
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-03-09 19:12:58 -0300

    Pesquisa_Binaria

---
+++ Entendendo Algoritimos/Pesquisa_Binaria.js
@@ -0,0 +1,22 @@
+function pesquisa_binaria(lista, item) {
+    let baixo = 0
+    let alto = lista.length - 1
+    
+    while ( baixo <= alto ) {
+      const meio = Math.floor(( baixo + alto ) / 2)
+      const chute = lista[meio]
+
+      if (chute === item) {
+          return meio
+      } else if (chute > item) {
+          console.log(alto = meio - 1)
+      } else {
+          baixo = meio + 1
+      }
+    }
+    return null
+}
+
+const minha_lista = [ 1, 3, 5, 7, 9 , 77 ]
+
+console.log(pesquisa_binaria(minha_lista, 1))
